aba9d48cddc4632ed5a4d25ca7c3a1b38be099ba
Kishan Kumar <Kishan.Kumar@ingramcontent.com>
2017-04-14 09:09:15 +0530

    CompletableFuture Demo_4 Source code

---
+++ CompletableFuture Demo_4/src/com/infotech/client/CompletableFutureTest.java
@@ -0,0 +1,35 @@
+package com.infotech.client;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+public class CompletableFutureTest {
+
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+		
+		CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(()->"Beatiful");
+		 completableFuture = completableFuture.thenApply(s->s+" World!!!");
+		 
+		 String result = completableFuture.get();
+		 System.out.println(result);
+		 
+		 System.out.println("---------------------------------------------------");
+		 
+		 CompletableFuture<String> completableFuture2 = CompletableFuture.supplyAsync(()->"Beatiful");
+		 
+		 completableFuture2.thenAccept(s->{
+			 System.out.println("My Computation is done::"+s);
+		 });
+		 completableFuture2.get();
+		 
+		 CompletableFuture<String> completableFuture3 = CompletableFuture.supplyAsync(()->"Beatiful");
+		 
+		 completableFuture3.thenRun(()->System.out.println("My Task is done.."));
+		 
+		 completableFuture3.get();
+		 
+		 
+		
+	}
+
+}
